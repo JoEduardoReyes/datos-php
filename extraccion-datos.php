2113da0fcc3ba3b2fbb6babc2f34a7f45a52cd6a
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-01-31 11:32:32 -0600

    Extracción de datos

---
+++ extraccion-datos.php
@@ -0,0 +1,34 @@
+<?php
+
+/* Extraer directamente datos especificos
+// $data = 'Estudio PHP';
+
+// // echo $data[0];
+
+// $post = "Lorem ipsum dolor sit amet, consectet adip  id, sed diam nonum placerat sad et d    Prometheus non    magna aliquet sed";
+
+// $extract = substr($post, 0, 20);
+
+// echo "$extract... [Ver mas]";
+
+*/
+
+/* convierte de string a array
+$data = 'JavaScript, PHP, Laravel';
+$tags = explode(', ', $data);
+
+var_dump($tags);
+
+*/
+
+/* convierte de array a string
+$courses = ['Javascript', 'PHP', 'Laravel'];
+
+echo implode(', ', $courses);
+*/
+
+/* quita los espacios
+$course = " Curso de PHP    ";
+
+echo trim($course);
+*/
